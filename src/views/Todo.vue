0776ce5f3ccfd113f0383f28e1f0eb4a64c47ebc
girotobial <abrobinson1907@gmail.com>
2021-02-01 22:53:06 +0000

    L19: mark task as done

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -2,14 +2,20 @@
   <div class="home">
     <v-list class="pt-0" flat>
       <div v-for="task in tasks" :key="task.id">
-        <v-list-item>
-          <template v-slot:default="{ active }">
+        <v-list-item
+          @click="doneTask(task.id)"
+          :class="{ 'blue lighten-5': task.done }"
+        >
+          <template v-slot:default>
             <v-list-item-action>
-              <v-checkbox :input-value="active" color="primary"></v-checkbox>
+              <v-checkbox :input-value="task.done" color="primary"></v-checkbox>
             </v-list-item-action>
 
             <v-list-item-content>
-              <v-list-item-title>{{ task.title }}</v-list-item-title>
+              <v-list-item-title
+                :class="{ 'text-decoration-line-through': task.done }"
+                >{{ task.title }}</v-list-item-title
+              >
             </v-list-item-content>
           </template>
         </v-list-item>
@@ -28,18 +34,27 @@ import { Component, Vue } from "vue-property-decorator";
       tasks: [
         {
           id: 1,
-          title: "Wake up"
+          title: "Wake up",
+          done: false
         },
         {
           id: 2,
-          title: "Get bananas"
+          title: "Get bananas",
+          done: false
         },
         {
           id: 3,
-          title: "Eat bananas"
+          title: "Eat bananas",
+          done: false
         }
       ]
     };
+  },
+  methods: {
+    doneTask(id: number) {
+      const task = this.tasks.filter(task => task.id === id)[0];
+      task.done = !task.done;
+    }
   }
 })
 export default class Home extends Vue {}

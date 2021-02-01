a99446b012339a0d99781c36f626211ffdb32ade
girotobial <abrobinson1907@gmail.com>
2021-02-01 21:50:57 +0000

    L13 setup todo view

---
+++ src/views/Todo.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="home pa-6">
+    <h1>Todo Page</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({})
+export default class Home extends Vue {}
+</script>

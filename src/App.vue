229450334581c733d1599eb62ab8ed4930e95f98
girotobial <abrobinson1907@gmail.com>
2021-02-01 21:38:10 +0000

    lesson 11: add drawer

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,30 @@
 <template>
   <v-app id="inspire">
     <v-navigation-drawer v-model="drawer" app>
-      <!--  -->
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="title">
+            Vuetify Todo
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            Best todo ever
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list dense nav>
+        <v-list-item v-for="item in items" :key="item.title" link>
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
     </v-navigation-drawer>
 
     <v-app-bar app>
@@ -18,6 +41,12 @@
 
 <script>
 export default {
-  data: () => ({ drawer: null })
+  data: () => ({
+    drawer: null,
+    items: [
+      { title: "Todo", icon: "mdi-view-dashboard" },
+      { title: "About", icon: "mdi-help-box" }
+    ]
+  })
 };
 </script>

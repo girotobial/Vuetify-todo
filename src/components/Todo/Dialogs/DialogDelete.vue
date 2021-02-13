97c88ea08aeb273bd26a57e4a6ad896e37710adc
girotobial <abrobinson1907@gmail.com>
2021-02-13 12:31:08 +0000

    L45: add dialog yes command

--- src/components/Todo/Dialogs/DialogDelete.vue
+++ src/components/Todo/Dialogs/DialogDelete.vue
@@ -4,15 +4,13 @@
       <v-card-title class="headline">
         Delete task?
       </v-card-title>
-      <v-card-text
-        >Are you sure you want to delete this task?</v-card-text
-      >
+      <v-card-text>Are you sure you want to delete this task?</v-card-text>
       <v-card-actions>
         <v-spacer></v-spacer>
         <v-btn text @click="dialog = false">
           No
         </v-btn>
-        <v-btn color="red" text @click="dialog = false">
+        <v-btn color="red" text @click="$store.dispatch('deleteTask', task.id)">
           Yes
         </v-btn>
       </v-card-actions>
@@ -21,7 +19,11 @@
 </template>
 
 <script>
-export default {};
+import Vue from "vue";
+
+export default Vue.extend({
+  props: ["task"]
+});
 </script>
 
 <style></style>

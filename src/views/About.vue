42c8bb77800bcf11cfd375f492fba1025cc32b6e
girotobial <abrobinson1907@gmail.com>
2021-02-13 23:10:53 +0000

    reference tech used

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,6 +3,8 @@
     <h1>About {{ $store.state.appTitle }}</h1>
     <p>
       An implementation of <a href="https://www.udemy.com/course/vuetify-vuejs-vuex/">Vuetify: Create an App with Vue JS and Vuex - in 5 Hours!</a>
+
+      Built with Vue2, Vuex, Vue router and Vuetify!
     </p>
   </div>
 </template>

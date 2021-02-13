a1984866ed0a72b834811361ddc2ed62ee6345da
girotobial <abrobinson1907@gmail.com>
2021-02-13 16:50:19 +0000

    use environment variables to control app title

--- src/router/index.ts
+++ src/router/index.ts
@@ -28,7 +28,7 @@ const router = new VueRouter({
 });
 
 router.beforeEach((to, from, next) => {
-  document.title = `Vuetifiy Todo - ${to.name}`;
+  document.title = `${process.env.VUE_APP_TITLE} - ${to.name}`;
   next();
 });
 
